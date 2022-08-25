23f40611a0e41b3b9806037493cf0d01bc33e498
Emilio-Fagiani <110248283+Emilio-Fagiani@users.noreply.github.com>
2022-08-25 17:04:09 +0200

    Update CoffeeShop.java
    Correction entity

--- src/CoffeeShop.java
+++ src/CoffeeShop.java
@@ -1,10 +1,5 @@
 public class CoffeeShop extends Shop{
 
-private String name;
-private Employee employee;
-private DayEnum day;
-private String numberToContact;
-
     public CoffeeShop(String name, String address, String numberToContact, String webSite, Employee employee, DayEnum day) {
         super(name, address, numberToContact, webSite, employee, day);
     }

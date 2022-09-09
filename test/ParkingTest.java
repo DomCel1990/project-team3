28025a9a855ea37f1e644abe4a0d8161074f3445
Emilio <emi00@gmail.com>
2022-09-09 11:08:48 +0200

    feat: Correction Parking test

--- test/ParkingTest.java
+++ test/ParkingTest.java
@@ -4,24 +4,25 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ParkingTest {
 
     @Test
     void placeParking() {
         List<Car> cars= new ArrayList<>();
-        List<Car> cars1= new ArrayList<>();
         for (int i = 0; i <20 ; i++) {
             cars.add(i, new Car());
         }
+        Parking p= new Parking(500,cars);
+        Assertions.assertEquals("Places: 480",p.placeParking());
+    }
+
+    @Test
+    void placeParking2(){
+        List<Car> cars1= new ArrayList<>();
         for (int i = 0; i <500 ; i++) {
             cars1.add(i, new Car());
         }
-        Parking p= new Parking(500,cars);
         Parking p1= new Parking(500,cars1);
-
-        Assertions.assertEquals("Places: 480",p.placeParking());
         Assertions.assertEquals("Parking is full",p1.placeParking());
     }
 
@@ -29,6 +30,6 @@ class ParkingTest {
     void maxPlaces() {
         List<Car> cars= new ArrayList<>();
         Parking p = new Parking(500,cars);
-        p.maxPlaces();
+        Assertions.assertEquals(500, p.maxPlaces());
     }
 }
